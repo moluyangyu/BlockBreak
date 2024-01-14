9dda17302d27aedcf1e783a71311593a9341abda
moluyangyu <985286227@qq.com>
2024-01-15 00:24:15 +0800

    合并内容

---
+++ BlockBreak/Assets/Script/BlockEliminator/BlockEliminator.cs
@@ -0,0 +1,217 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockType
+{
+    turn,
+    switchSpeed,
+    switchStop,
+    landform,
+    ui,
+    easterEgg
+}
+
+public enum BlockState
+{
+    original=0,
+    activated=1
+}
+
+public class BlockEliminator : MonoBehaviour
+{
+
+    private const int ELIMINATE_COUNT = 7;
+    private const int BLOCK_LAYER = 1 << 8;
+
+    public float moveDuration;
+
+    private int activatedBlocksCount = 0;
+    private int actingBlocksCount = 0;
+    private GameObject eliminateArea;
+    //private Vector3[] eliminatePos = new Vector3[3]; 
+    private List<GameObject> activatedBlocks = new List<GameObject>();
+
+    private List<GameObject> blockOriginPoints = new List<GameObject>();
+    private GameObject[] eliminatePoints = new GameObject[ELIMINATE_COUNT];
+    private bool[] eliminatejudge = new bool[ELIMINATE_COUNT];
+
+    private void Awake()
+    {
+        Initialize();
+        GetEliminateArea();
+    }
+
+    private void GetEliminateArea()
+    {
+        eliminateArea = GameObject.Find("BlockEliminateArea");
+        for (int i = 0; i < ELIMINATE_COUNT; i++)
+        {
+            //eliminatePos[i] = eliminateArea.transform.GetChild(i).position;
+            eliminatePoints[i] = eliminateArea.transform.GetChild(i).gameObject;
+        }
+        
+    }
+
+    private void Initialize()
+    {
+        for (int i = 0; i < ELIMINATE_COUNT; i++)
+        {
+            eliminatejudge[i] = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckClick();
+    }
+
+    private void CheckClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray check = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(check.origin.x, check.origin.y), Vector2.down, 0.01f, BLOCK_LAYER);
+            if (hit.collider)
+            {
+                GameObject nowBlock = hit.collider.gameObject;
+                MoveBlock(nowBlock);
+            }
+        }
+    }
+
+    private void MoveBlock(GameObject block)
+    {
+        BlockStatus status = block.GetComponent<BlockStatus>();
+        if(!block.GetComponent<BlockStatus>().moving)
+        {
+            if (status.state == BlockState.original && actingBlocksCount < ELIMINATE_COUNT)
+            {
+                status.state = BlockState.activated;
+                activatedBlocks.Add(block);
+                actingBlocksCount++;
+                StartCoroutine(Move(block, block.transform.position, moveDuration, 1));
+            }
+            else if(status.state == BlockState.activated)
+            {
+                status.state = BlockState.original;
+                activatedBlocks.Remove(block);
+                actingBlocksCount--;
+                StartCoroutine(Move(block, block.transform.position, moveDuration, -1));
+            }
+        }
+    }
+
+    private void TryEliminate()
+    {
+        BlockType type = activatedBlocks[0].GetComponent<BlockStatus>().type;
+        for(int i = 1; i < activatedBlocksCount; i++)
+        {
+            if (type == activatedBlocks[i].GetComponent<BlockStatus>().type)
+                continue;
+            //foreach(GameObject block in activatedBlocks)
+            //{
+            //    BlockStatus status = block.GetComponent<BlockStatus>();
+            //    status.state = BlockState.original;
+
+            //    StartCoroutine(Move(block, block.transform.position, moveDuration, -1));
+            //}
+            
+            return;
+        }
+        activatedBlocks.Clear();
+        //activatedBlocksCount=0;
+        actingBlocksCount = 0;
+        Eliminate();
+    }
+
+    private void Eliminate()
+    {
+        Debug.Log("Eliminate!");
+        BlockType type = activatedBlocks[0].GetComponent<BlockStatus>().type;
+        foreach (GameObject block in activatedBlocks)
+        {
+            blockOriginPoints.Add(block.GetComponent<BlockStatus>().refreshPoint);
+            Destroy(block);
+        }
+        //foreach (GameObject point in blockOriginPoints)
+        //{
+        //    BlockRefresher.Instance.CreateBlock(point);
+        //}
+        activatedBlocks.Clear();
+        activatedBlocksCount=0;
+        actingBlocksCount = 0;
+        //event
+        switch (type)
+        {
+            case BlockType.turn:
+                PlayerController.Instance.Turn();
+                break;
+            case BlockType.switchSpeed:
+                PlayerController.Instance.SwitchSpeed();
+                break;
+            case BlockType.switchStop:
+                PlayerController.Instance.SwitchStop();
+                break;
+        }
+    }
+
+    private IEnumerator Move(GameObject obj, Vector3 startPos, float duration, int dir)
+    {
+        BlockStatus status = obj.GetComponent<BlockStatus>();
+        status.moving = true;
+        float elapsedTime = 0f;
+        if (dir == 1)
+        {
+            status.ePointIndex = FindEliminatePoint();
+            status.eliminatePoint = eliminatePoints[status.ePointIndex];
+            eliminatejudge[status.ePointIndex] = false;
+            while ((obj.transform.position - status.eliminatePoint.transform.position).magnitude > 0.01f)
+            {
+                float t = elapsedTime / duration;
+                obj.transform.position = Vector3.Lerp(startPos, status.eliminatePoint.transform.position, t);
+                yield return null;
+                elapsedTime += Time.deltaTime;
+            }
+        }
+        else if (dir == -1)
+        {
+            eliminatejudge[status.ePointIndex] = true;
+            while ((obj.transform.position - status.refreshPoint.transform.position).magnitude > 0.01f)
+            {
+                float t = elapsedTime / duration;
+                obj.transform.position = Vector3.Lerp(startPos, status.refreshPoint.transform.position, t);
+                yield return null;
+                elapsedTime += Time.deltaTime;
+            }
+        }
+        
+        status.moving = false;
+        activatedBlocksCount += dir;
+        if (activatedBlocksCount == ELIMINATE_COUNT)
+        {
+            TryEliminate();
+        }
+    }
+
+    private int FindEliminatePoint()
+    {
+        for(int i = 0; i< ELIMINATE_COUNT; i++)
+        {
+            if (eliminatejudge[i] == true)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
---
+++ BlockBreak/Assets/Script/BlockEliminator/BlockRefresher.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class BlockRefresher : MonoBehaviour
+{
+    public GameObject blockPrefab;
+
+    private GameObject refreshArea;
+    private GameObject[] refreshPoints = new GameObject[99];
+
+
+    public Sprite turn;
+    public Sprite switchSpeed;
+    public Sprite switchStop;
+    public Sprite ui;
+    public Sprite landform;
+    public Sprite easterEgg;
+
+    private static BlockRefresher instance;
+    public static BlockRefresher Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = GameObject.Find("BlockRefreshArea").GetComponent<BlockRefresher>();
+            return instance;
+        }
+    }
+    
+    private void Awake()
+    {
+        GetRefreshArea();
+    }
+
+    private void GetRefreshArea()
+    {
+        refreshArea = GameObject.Find("BlockRefreshArea");
+        for (int i = 0; i < refreshArea.transform.childCount; i++)
+        {
+            refreshPoints[i] = refreshArea.transform.GetChild(i).gameObject;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //RefreshAll();
+    }
+
+    private void RefreshAll()
+    {
+        GameObject[] blockArray = GameObject.FindGameObjectsWithTag("Block");
+        foreach (GameObject block in blockArray)
+        {
+            Destroy(block);
+        }
+        for (int i = 0;i < refreshArea.transform.childCount; i++)
+        {
+            CreateBlock(refreshPoints[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateBlock(GameObject point)
+    {
+        GameObject block = UnityEngine.Object.Instantiate(blockPrefab);
+        block.transform.position = point.transform.position;
+        BlockStatus status = block.GetComponent<BlockStatus>();
+        status.refreshPoint = point;
+        status.state = BlockState.original;
+        status.moving = false;
+        status.type = (BlockType)UnityEngine.Random.Range(0, Enum.GetNames(typeof(BlockType)).Length);
+        switch (status.type)
+        {
+            case BlockType.turn:
+                status.GetComponent<SpriteRenderer>().sprite = turn;
+                break;
+            case BlockType.switchSpeed:
+                status.GetComponent<SpriteRenderer>().sprite = switchSpeed;
+                break;
+            case BlockType.switchStop:
+                status.GetComponent<SpriteRenderer>().sprite = switchStop;
+                break;
+            case BlockType.ui:
+                status.GetComponent<SpriteRenderer>().sprite = ui;
+                break;
+            case BlockType.landform:
+                status.GetComponent<SpriteRenderer>().sprite = landform;
+                break;
+            case BlockType.easterEgg:
+                status.GetComponent<SpriteRenderer>().sprite = easterEgg;
+                break;
+        }
+
+    }
+
+}
