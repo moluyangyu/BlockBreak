3d8e4b2735db13b0e9022464f947c0144c28d054
FoxHanshuang <yiboy.qys@foxmail.com>
2024-01-17 15:21:14 +0800

    npc

---
+++ BlockBreak/Assets/Script/PlayerController/GuardController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardController : MonoBehaviour
+{
+    public float speed;
+    private int dir = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(dir * speed * Time.deltaTime, 0f, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if(collision.gameObject.tag == "Turn")
+        {
+            dir = -dir;
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, 0f);
+        }
+    }
+}
