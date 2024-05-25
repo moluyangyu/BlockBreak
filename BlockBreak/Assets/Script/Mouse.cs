01294499fd6faa915e1ef6199eec6b779c4a2005
moluyangyu <985286227@qq.com>
2024-05-25 14:48:04 +0800

    添加鼠标光标

---
+++ BlockBreak/Assets/Script/Mouse.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image hammer;  //定义一个图片
+
+    void Start()
+    {
+       // DontDestroyOnLoad(this);
+        Cursor.visible = false;
+        hammer = this.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 0;
+        hammer.rectTransform.position = mousePosition; //图片的位置跟随鼠标的位置
+    }
+}
